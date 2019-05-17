4442b282f1dc2ff7fcf11fa2a553ba75a95ffe8f
murat atalay <atalaymurat@msn.com>
2019-05-17 23:45:37 +0300

    user not sign in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
     #"/app/views/devise/sessions/new.html.erb"
-    redirect_to new_user_session_path
+    if not user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end

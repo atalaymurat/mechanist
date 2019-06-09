acb238e2363d761ac728b2ec13686321c4d8c6ca
murat atalay <atalaymurat@msn.com>
2019-06-10 02:40:53 +0300

    email policy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :registerable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  has_many :people
+  has_many :emails
 
   def username
     return self.email.split('@')[0].capitalize

f35ca3d22b8e5e3bc3eb979665e36a8b7100fac6
murat atalay <atalaymurat@msn.com>
2019-06-23 22:30:20 +0300

    Options for select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
       "alert-warning"  
     end
   end
+
+  def options_for_phone_types
+  ["Mobile", "Work", "Home", "Private"]
+  end
+
+  def options_for_positions
+    ["Manager", "Shareholder", "Co Founder", "Co Owner", "Owner", "Director","Sales Manager", "Logistics Manager", "Technical Manager", "Production Manager", "Marketting Manager", "Factory Manager", "Employee", "Other", "President", "Coordinator", "CEO", "Member of Board" ].sort
+  end
 end
--- app/models/connected.rb
+++ app/models/connected.rb
@@ -1,4 +1,10 @@
 class Connected < ApplicationRecord
   belongs_to :person
   belongs_to :company
+  before_save :default_values
+
+  def default_values
+    self.position = "Employee" if self.position.nil?
+  end
+
 end
